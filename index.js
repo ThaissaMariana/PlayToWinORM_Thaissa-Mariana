50dc70476ccfdb2b173b51f519377b86ef0dd6de
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-03-25 14:27:30 -0400

    Commit - Aula 25-03

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 require("dotenv").config();
 const conn = require("./db/conn");
 
+const Usuario = require("./models/Usuario");
+
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+app.get("/usuarios/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
+ });
+
+app.post("/usuarios/novo", async (req, res) => { 
+   const nickname = req.body.nickname;
+   const nome = req.body.nome;
+
+   const dadosUsuario = {
+    nickname,
+    nome,
+   };
+
+   const usuario = await Usuario.create(dadosUsuario);
+
+   res.send("Usuário inserido sob o id " + usuario.id);
+});
+
+app.listen(8000);
+
 conn
-.authenticate()
+.sync()
 .then(() => {
     console.log("Conectado com sucesso!");
-}).catch( (err)=>{
+})
+.catch( (err)=> {
     console.log("Ocorreu um erro: " + err)
 });
